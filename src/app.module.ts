46b8ceba6f3aaf9af0b994ce7c8ec748a8d261a5
Nicholas Konar <nkonar@protonmail.com>
2024-10-24 22:11:13 -0400

    Setup apollo server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EdgesModule } from './edges/edges.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
 
 @Module({
-  imports: [EdgesModule],
+  imports: [
+    EdgesModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      playground: true,
+    }),
+],
   controllers: [AppController],
   providers: [AppService],
 })
